80a7a923451e3ff4e6ffa5cae6c240aedc65c855
Kostya <ckutls228@gmail.com>
2023-11-12 22:23:19 +0300

    added logo on page

--- luckydiamond/src/pages/HeaderElement.vue
+++ luckydiamond/src/pages/HeaderElement.vue
@@ -1,13 +1,17 @@
 <template>
   <header class="header">
     <div class="header__content">
-      <div class="header__logo"></div>
+      <div class="header__logo">
+        <img src="../assets/logo.svg" alt="LuckyDiamond">
+      </div>
     </div>
   </header>
 </template>
 
 <script>
-
+export default {
+  name: 'Header-Element-page'
+}
 </script>
 
 <style>
