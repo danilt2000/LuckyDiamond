d08198a8095783977369ea0909ce1c79a42b09cf
Kostya <ckutls228@gmail.com>
2023-11-12 23:03:52 +0300

    added btns on aside bar

--- luckydiamond/src/pages/AsidebarElement.vue
+++ luckydiamond/src/pages/AsidebarElement.vue
@@ -1,7 +1,12 @@
 <template>
   <aside class="bar">
     <div class="bar__content">
-
+      <div class="bar__buttons">
+        <a href="#" class="bar__button--home"> <img src="../assets/icons/icon-home-button.png"> </a>
+        <div class="bar__settings">
+          <a href="#" class="bar__button--settings"> <img src="../assets/icons/icon-settings-button.png"> </a>
+        </div>
+      </div>
     </div>
   </aside>
 </template>
@@ -20,5 +25,19 @@ export default {
   position: fixed;
   top: 0;
   left: 0;
+}
+.bar__content  {
+  padding-top: 26px;
+  justify-content: center;
+  display: flex;
+  align-items: center;
+  text-align: center;
+}
+.bar__settings {
+  position: absolute;
+  bottom: 0;
+}
+.bar__settings .bar__button--settings img {
+  margin-bottom: 15px;
 }
 </style>
