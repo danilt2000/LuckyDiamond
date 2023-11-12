1c011c4311febe90ec16fc311abec019939efe16
Kostya <ckutls228@gmail.com>
2023-11-12 22:33:13 +0300

    added file for the future bar

---
+++ luckydiamond/src/pages/AsidebarElement.vue
@@ -0,0 +1,17 @@
+<template>
+  <aside class="bar">
+    <div class="bar__content">
+
+    </div>
+  </aside>
+</template>
+
+<script>
+export default {
+  name: 'AsideBar-Element'
+}
+</script>
+
+<style>
+
+</style>
