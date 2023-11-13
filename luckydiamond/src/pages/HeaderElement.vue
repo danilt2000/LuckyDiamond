35aede56508014907ec92fe350f74893453eb341
Kostya <ckutls228@gmail.com>
2023-11-13 21:16:27 +0300

    added bg for header element

--- luckydiamond/src/pages/HeaderElement.vue
+++ luckydiamond/src/pages/HeaderElement.vue
@@ -22,8 +22,35 @@ export default {
   width: 100%;
   z-index: 1;
   user-select: none;
+  overflow: hidden;
+}
+.header::before {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(223, 133, 255, 0.60);
+  filter: blur(115px);
+  z-index: -1;
+}
+.header::after {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 22%;
+  width: 60%;
+  height: 100%;
+  background: #7000FF;
+  filter: blur(190px);
+  z-index: -2;
 }
 .header__content {
-  padding: 10px;
+  position: relative;
+  z-index: 1;
+}
+.header__logo {
+  padding: 26px 58px;
 }
 </style>
