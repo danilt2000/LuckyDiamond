196839cb38fbd1c625037803edeb631042179a31
Kostya <ckutls228@gmail.com>
2023-11-14 23:17:15 +0300

    added mocks file

---
+++ luckydiamond/src/mocks/GameModes.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        id: 1,
+        title: 'slots'
+    },
+    {
+        id: 2,
+        title: 'double game'
+    },
+    {
+        id: 3,
+        title: 'crash game'
+    },
+    {
+        id: 4,
+        title: 'jackpot game'
+    },
+    {
+        id: 5,
+        title: 'mines game'
+    }
+]
