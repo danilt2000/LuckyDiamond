a1c39b7e22b944bb1f278835c1242e99ed154381
Kostya <ckutls228@gmail.com>
2023-11-15 22:14:13 +0300

    changed mocks/gamemode, added param players

--- luckydiamond/src/mocks/GameModes.js
+++ luckydiamond/src/mocks/GameModes.js
@@ -1,22 +1,27 @@
 export default [
     {
         id: 1,
-        title: 'slots'
+        title: 'slots',
+        players: 0
     },
     {
         id: 2,
-        title: 'double game'
+        title: 'double game',
+        players: 0
     },
     {
         id: 3,
-        title: 'crash game'
+        title: 'crash game',
+        players: 0
     },
     {
         id: 4,
-        title: 'jackpot game'
+        title: 'jackpot game',
+        players: 0
     },
     {
         id: 5,
-        title: 'mines game'
+        title: 'mines game',
+        players: 0
     }
 ]
