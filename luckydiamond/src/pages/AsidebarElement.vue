1365a4a996ef856470c629afdd07ff84dd12f238
Kostya <ckutls228@gmail.com>
2023-11-19 13:35:19 +0300

    changed icons and icons styles in aside bar, menu

--- luckydiamond/src/pages/AsidebarElement.vue
+++ luckydiamond/src/pages/AsidebarElement.vue
@@ -2,18 +2,18 @@
   <aside class="menu">
     <div class="menu__content">
       <div class="menu__btn--home">
-        <a href="#"><img width="73" height="73" src="../assets/icons-home/home-icon.svg"></a>
+        <a href="#"><img width="73" height="73" src="../assets/icons-menu/home-icon.svg"></a>
       </div>
       <div class="menu__btns--gamemodes">
         <div class="menu__content--gamemodes">
-          <a href="#"><img src="../assets/icons-home/slots-icon.svg"></a>
-          <a href="#"><img src="../assets/icons-home/crush-icon.svg"></a>
-          <a href="#"><img src="../assets/icons-home/case-icon.png"></a>
-          <a href="#"><img src="../assets/icons-home/bomb-icon.svg"></a>
+          <a href="#"><img src="@/assets/icons-menu/slots-icon.svg"></a>
+          <a href="#"><img src="@/assets/icons-menu/crush-icon.svg"></a>
+          <a href="#"><img src="@/assets/icons-menu/case-icon.png"></a>
+          <a href="#"><img src="@/assets/icons-menu/bomb-icon.svg"></a>
         </div>
       </div>
       <div class="menu__btn--settings">
-        <a href="#"><img width="73" height="73" src="../assets/icons-home/settings-icon.svg"></a>
+        <a href="#"><img width="73" height="73" src="../assets/icons-menu/settings-icon.svg"></a>
       </div>
     </div>
   </aside>
