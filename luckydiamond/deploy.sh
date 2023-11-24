32b55d4b5c5a028aaed28462defbfca51aa0d324
Hepatica <danil.tkachenko@getmore.cz>
2023-11-24 03:44:49 +0100

    deploy

--- luckydiamond/deploy.sh
+++ luckydiamond/deploy.sh
@@ -2,26 +2,26 @@
 
 # abort on errors
 set -e
-
 # build
 npm run build
 
 # navigate into the build output directory
 cd dist
+# git checkout main
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
+# git init
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+git push -f git@github.com:danilt2000/danilt2000.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git pull origin
-git push -f git@github.com:danilt2000/LuckyDiamond.git main:gh-pages
+# git push -f git@github.com:danilt2000/LuckyDiamond.git main:gh-pages
 # git subtree push --prefix dist origin gh-pages
 
 cd -
--- luckydiamond/vue.config.js
+++ luckydiamond/vue.config.js
@@ -1,7 +1,11 @@
 // vue.config.js file to be placed in the root of your repository
 
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === 'production'
+//     ? '/luckydiamond/'
+//     : '/'
+// }
+
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/LuckyDiamond/'
-    : '/'
+  publicPath: '/'
 }
