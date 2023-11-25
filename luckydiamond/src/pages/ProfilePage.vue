ca303e8637f77617686312f9a45190f4be4ed4b1
Kostya <ckutls228@gmail.com>
2023-11-25 23:44:03 +0300

    added styles for profile-user info in profile page

--- luckydiamond/src/pages/ProfilePage.vue
+++ luckydiamond/src/pages/ProfilePage.vue
@@ -8,7 +8,15 @@
 
   <section class="profile">
     <div class="profile__content">
-
+      <img class="profile__user--img" src="@/assets/icons-test/person-icon-profile.png">
+      <div class="profile__card">
+        <h1>{{ username }}</h1>
+        <h2><img src="@/assets/icons-profile/icon-diamond-ore.png">{{ balance }}</h2>
+      </div>
+      <div class="profile__btns--payments">
+        <a href="#" class="text-btn btn-bg btn-margin btn-display">Пополнить <img class="deposit-icon" src="@/assets/icons-profile/icon-deposit.svg"></a>
+        <a href="#" class="withdraw text-btn btn-bg btn-display">Вывести <img class="withdraw-icon" src="@/assets/icons-profile/icon-withdraw.svg"></a>
+      </div>
     </div>
     <div class="payments">
       <div class="payments__content">
@@ -26,7 +34,13 @@ import HeaderComponent from "@/components/HeaderComponent.vue";
 import '@/assets/css/PagesStyles/profile.css'
 
 export default {
-  components: { HeaderComponent, AsideBarComponent, ChatComponent }
+  components: { HeaderComponent, AsideBarComponent, ChatComponent },
+  data() {
+    return {
+      username: 'Artemka',
+      balance: 25000
+    }
+  }
 
 }
 </script>
