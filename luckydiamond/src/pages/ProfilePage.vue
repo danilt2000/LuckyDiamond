0443f1eeb9bc21e4b4e40e6c8ff6a75dd1e530cb
Kostya <ckutls228@gmail.com>
2023-11-25 22:33:00 +0300

    added template for profile, add components

--- luckydiamond/src/pages/ProfilePage.vue
+++ luckydiamond/src/pages/ProfilePage.vue
@@ -1,11 +1,21 @@
 <template>
   <div class="content__grid-profile">
-    <h1>profile</h1>
+    <aside-bar-component></aside-bar-component>
+
+    <chat-component></chat-component>
+
+    <header-component></header-component>
+
   </div>
 </template>
 
 <script>
+import AsideBarComponent from "@/components/AsidebarComponent.vue";
+import ChatComponent from "@/components/ChatComponent.vue";
+import HeaderComponent from "@/components/HeaderComponent.vue";
+
 export default {
+  components: { HeaderComponent, AsideBarComponent, ChatComponent }
 
 }
 </script>
