1a3312c809369e9a72f30a7903aee800a6c8f32c
Kostya <ckutls228@gmail.com>
2023-11-26 18:35:39 +0300

    added styles for mobile-header

--- luckydiamond/src/pages/adaptive-pages/HomemobilePage.vue
+++ luckydiamond/src/pages/adaptive-pages/HomemobilePage.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="content-grid__mobile-home">
     <header class="header-mobile">
-      <h1>header</h1>
+      <div class="header-mobile__content">
+        <div class="header-mobile__logo">
+          <img src="@/assets/icons-adaptive/home-adaptive/logo.svg" alt="LuckyDiamond Play Now">
+        </div>
+        <div class="header-mobile__nav">
+          <div class="balance-mobile__card card-balance">
+            <h2><img src="@/assets/icons-adaptive/home-adaptive/icon-diamond-ore.png">999</h2>
+            <a href="#"><img src="@/assets/icons-adaptive/home-adaptive/icon-deposit.png"></a>
+          </div>
+        </div>
+      </div>
     </header>
     <main class="main-mobile">
       <h1>main</h1>
