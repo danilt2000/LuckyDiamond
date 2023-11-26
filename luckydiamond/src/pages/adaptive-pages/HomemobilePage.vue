b246e5adb644d5789e8d74e8b500f79107926dd9
Kostya <ckutls228@gmail.com>
2023-11-26 18:15:34 +0300

    building grid for mobile v.

--- luckydiamond/src/pages/adaptive-pages/HomemobilePage.vue
+++ luckydiamond/src/pages/adaptive-pages/HomemobilePage.vue
@@ -1,10 +1,20 @@
 <template>
   <div class="content-grid__mobile-home">
-    <h1>mobile</h1>
+    <header class="header-mobile">
+      <h1>header</h1>
+    </header>
+    <main class="main-mobile">
+      <h1>main</h1>
+    </main>
+    <footer class="footer-mobile">
+      <h1>footer</h1>
+    </footer>
   </div>
 </template>
 
 <script>
+import '@/assets/css/PagesStyles/adaptive-pages/homemobile.css'
+
 export default {
 
 }
