a9f0ad9a10ed764a65145826d4e0ee920fc95c30
Kostya <ckutls228@gmail.com>
2023-12-03 08:53:19 +0300

    added styles for text and input in saperpage

--- luckydiamond/src/pages/games-pages/SapergamePage.vue
+++ luckydiamond/src/pages/games-pages/SapergamePage.vue
@@ -8,14 +8,14 @@
 
     <section class="saper">
       <div class="saper-start">
-        <div class="saper-start__content">
+        <div class="saper-start__content title-saper padding-elements">
           <h2>Сапёр</h2>
           <p>Находи изумруды на поле и избегай кристалы</p>
           <div class="saper-start__choises crystals">
             <h3>Количество кристалов</h3>
             <div class="crystals__choises crystals-input__margin">
               <img class="crystal-icon" width="25" height="25" src="@/assets/icons-games/saper-game/icon-crystall-saper.svg">
-              <input type="text">
+              <input type="number">
             </div>
           </div>
         </div>
