6b28e3b39c0b129748eedf14e1ee42a37274010a
Kostya <ckutls228@gmail.com>
2023-12-06 11:00:17 +0300

    changed template and added styles for modal payments in paymentsmodal component

--- luckydiamond/src/components/PaymentsModal.vue
+++ luckydiamond/src/components/PaymentsModal.vue
@@ -8,23 +8,35 @@
       <div class="deposit-input deposit-text deposit-icon-diamond deposit-icon__input">
         <h3>Сумма пополнения</h3>
         <img src="@/assets/icons-games/saper-game/icon-diamond-ore-saper.png">
-        <input type="number">
+        <input v-model="amount" type="number">
         <div class="deposit-btns">
-
+          <ul class="display-btns btns-style-diamonds">
+            <li v-for="number in PaymentsModalNumbers" :key="number">
+              <button>{{ number.diamonds }}</button>
+            </li>
+          </ul>
+        </div>
+        <div class="deposit-promocode deposit-promocode__padding--input">
+          <h3>Промокод</h3>
+          <input type="text">
+        </div>
+        <div class="btn-deposit btn-style-payments">
+          <button type="submit" @click="detectorMethod('dep')">Пополнить</button>
         </div>
       </div>
     </div>
-    <div v-else class="payments-modal__withdraw payments__input--color payments__margin payments__button">
-      <div class="count-withdraw">
-        <label>Введите сумму для снятия</label>
+    <div v-else class="payments-modal__withdraw deposit-text withdraw-input">
+      <div class="count-withdraw deposit-icon-diamond">
+        <h3>Сумма вывода</h3>
+        <img src="@/assets/icons-games/saper-game/icon-diamond-ore-saper.png">
         <input v-model="amount" type="number">
       </div>
-      <div class="number-card">
-        <label>Введите номер карты</label>
+      <div class="number-card deposit-promocode__padding--input">
+        <h3>Введите номер карты</h3>
         <input v-model="card" type="number">
       </div>
-      <div class="payments-modal__content--btn">
-        <button type="submit" @click="detectorMethod('with')">Подтвердить</button>
+      <div class="btn-withdraw btn-style-payments">
+        <button type="submit" @click="detectorMethod('with')">Вывод</button>
       </div>
     </div>
   </div>
@@ -32,6 +44,7 @@
 
 <script>
 import '@/assets/css/ComponentsStyles/payments-modal.css'
+import PaymentsModalNumbers from "@/mocks/PaymentsModalNumbers";
 
 export default {
   props: [
@@ -41,6 +54,7 @@ export default {
     return {
       amount: 1,
       card: 0,
+      PaymentsModalNumbers
     }
   },
   methods: {
