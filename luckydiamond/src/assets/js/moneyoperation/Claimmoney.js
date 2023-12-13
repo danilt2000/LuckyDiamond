4ae4be04c49cef2733b6e2b1f1af95ad94084eec
Hepatica <danil.tkachenko@getmore.cz>
2023-12-13 18:40:35 +0100

    change payment api call

--- luckydiamond/src/assets/js/moneyoperation/Claimmoney.js
+++ luckydiamond/src/assets/js/moneyoperation/Claimmoney.js
@@ -1,14 +1,39 @@
-import { Post } from '../rest/RestMethods.js';
-// import { BackendApiUrl } from '@/properties/Сonfig.js';
+// import { Post } from '../rest/RestMethods.js';
+import { BackendApiUrl } from '@/properties/Сonfig.js';
 
 export async function GettingMoneyOperation(amount) {
-    try {
-        const response = await Post('https://spsystemcore20231122004605.azurewebsites.net/api/payment/donate', { request: amount })
+    // try {
+    //     const response = await Post('https://spsystemcore20231122004605.azurewebsites.net/api/payment/donate', { request: amount })
 
+    //     console.log(response)
+    //     return response
+    // }
+    // catch (e) {
+    //     console.error('Error in ClaimMoney method: ', e)
+    // }
+
+
+    const data = {
+        Amount: amount
+    };
+    try {
+        const response = await fetch(`${BackendApiUrl}/api/payment/donate`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+            redirect: "follow"
+        });
+
+        if (!response.ok) {
+            console.log("Fetch error:", response.status);
+        }
         console.log(response)
-        return response
+
+        return await response.json();
+    } catch (error) {
+        console.log("Fetch error:", error);
     }
-    catch (e) {
-        console.error('Error in ClaimMoney method: ', e)
-    }
+
 }
