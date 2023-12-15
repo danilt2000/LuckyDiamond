ddbf55ddee1a748c9985d6a08eadecd3250e285d
Kostya <ckutls228@gmail.com>
2023-12-15 21:20:29 +0300

    refactor footer mobile

--- luckydiamond/src/components/adaptive-components/MenuMobileComponent.vue
+++ luckydiamond/src/components/adaptive-components/MenuMobileComponent.vue
@@ -1,20 +1,12 @@
 <template>
   <footer class="footer-mobile">
     <div class="footer-mobile__content">
-      <div class="footer-mobile__block chat-icon">
-        <a href="#">
-          <img src="@/assets/icons-adaptive/home-adaptive/icon-chat.svg">
-        </a>
-      </div>
-      <div class="footer-mobile__block home-icon">
-        <a href="#" @click="$router.push({ name: 'home' })">
-          <img src="@/assets/icons-adaptive/home-adaptive/icon-home.svg">
-        </a>
-      </div>
-      <div class="footer-mobile__block settings-icon">
-        <a href="#">
-          <img width="" src="@/assets/icons-adaptive/home-adaptive/icon-settings.png">
-        </a>
+      <div class="footer-mobile__icons icons-size">
+        <img class="icon-start" src="@/assets/icons-adaptive/home-adaptive/icon-home-menu.png">
+        <img class="icon-center" src="@/assets/icons-adaptive/home-adaptive/icon-wallet-menu.svg">
+        <img class="icon-center" src="@/assets/icons-adaptive/home-adaptive/icon-chat-menu.png">
+        <img class="icon-center" src="@/assets/icons-adaptive/home-adaptive/icon-info-menu.png">
+        <img class="icon-end" src="@/assets/icons-adaptive/home-adaptive/icon-menu.png">
       </div>
     </div>
   </footer>
