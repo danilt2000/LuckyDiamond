c5363e5a81a99a7fb556ba5e4cef13bbb9c704f5
Kostya <ckutls228@gmail.com>
2023-12-15 20:56:59 +0300

    refactor header-mobile

--- luckydiamond/src/components/adaptive-components/HeaderMobileComponent.vue
+++ luckydiamond/src/components/adaptive-components/HeaderMobileComponent.vue
@@ -4,14 +4,19 @@
       <div class="header-mobile__logo">
         <img src="@/assets/icons-adaptive/home-adaptive/logo.svg" alt="LuckyDiamond Play Now">
       </div>
-      <div class="header-mobile__balance">
-        <div class="header-mobile__balance--content">
-          <div class="header-mobile__card card-display card-text card-img">
-            <h2><img src="@/assets/icons-adaptive/home-adaptive/icon-diamond-ore.png">999</h2>
+      <div class="header-mobile__end">
+        <div class="header-mobile__balance">
+          <div class="header-mobile__balance--content">
+            <div class="header-mobile__card card-display card-text card-img">
+              <h2><img src="@/assets/icons-adaptive/home-adaptive/icon-diamond-ore.png">999</h2>
+            </div>
+            <div class="header-mobile__btn--balance btn-card btn-card__margin">
+              <img src="@/assets/icons-adaptive/home-adaptive/icon-wallet.svg">
+            </div>
           </div>
-          <div class="header-mobile__btn--balance btn-card btn-card__margin">
-            <a href="#"><img src="@/assets/icons-adaptive/home-adaptive/icon-deposit.png"></a>
-          </div>
+        </div>
+        <div class="header-mobile__user-icon user-icon">
+          <img src="@/assets/icons-test/person-icon-profile-userinfo.png">
         </div>
       </div>
     </div>
