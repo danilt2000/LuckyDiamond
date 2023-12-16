4960d1dbd60f3ac2dbbbe394ae97fae864efe04d
Kostya <ckutls228@gmail.com>
2023-12-16 14:50:58 +0300

    created profilemobile page template and grid

---
+++ luckydiamond/src/pages/adaptive-pages/ProfilemobilePage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="content-grid__mobile-profile">
+    <header-mobile-component></header-mobile-component>
+
+    <main class="profile-mobile">
+
+    </main>
+
+    <menu-mobile-component></menu-mobile-component>
+
+  </div>
+</template>
+
+<script>
+import HeaderMobileComponent from "@/components/adaptive-components/HeaderMobileComponent.vue";
+import MenuMobileComponent from "@/components/adaptive-components/MenuMobileComponent.vue";
+export default {
+  components: { HeaderMobileComponent, MenuMobileComponent }
+}
+</script>
+
+<style scoped>
+
+</style>
