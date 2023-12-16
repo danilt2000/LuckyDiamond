704090a5e12df2d017584cf050fc969243ac4932
Kostya <ckutls228@gmail.com>
2023-12-16 14:42:47 +0300

    changed template and margin for menu-mobile

--- luckydiamond/src/components/adaptive-components/MenuMobileComponent.vue
+++ luckydiamond/src/components/adaptive-components/MenuMobileComponent.vue
@@ -2,11 +2,11 @@
   <footer class="footer-mobile">
     <div class="footer-mobile__content">
       <div class="footer-mobile__icons icons-size">
-        <img class="icon-start" src="@/assets/icons-adaptive/home-adaptive/icon-home-menu.png">
-        <img class="icon-center" src="@/assets/icons-adaptive/home-adaptive/icon-wallet-menu.svg">
-        <img class="icon-center" src="@/assets/icons-adaptive/home-adaptive/icon-chat-menu.png">
-        <img class="icon-center" src="@/assets/icons-adaptive/home-adaptive/icon-info-menu.png">
-        <img class="icon-end" src="@/assets/icons-adaptive/home-adaptive/icon-menu.png">
+        <a href=""><img class="icon-start" src="@/assets/icons-adaptive/home-adaptive/icon-home-menu.png"></a>
+        <a href=""><img class="icon-center" src="@/assets/icons-adaptive/home-adaptive/icon-wallet-menu.svg"></a>
+        <a href=""><img class="icon-center" src="@/assets/icons-adaptive/home-adaptive/icon-chat-menu.png"></a>
+        <a href=""><img class="icon-center" src="@/assets/icons-adaptive/home-adaptive/icon-info-menu.png"></a>
+        <a href=""><img class="icon-end" src="@/assets/icons-adaptive/home-adaptive/icon-menu.png"></a>
       </div>
     </div>
   </footer>
