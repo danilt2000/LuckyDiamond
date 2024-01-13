ad804c3dea15df631d3774b2c179b72bd4964998
Kostya <ckutls228@gmail.com>
2024-01-13 12:00:37 +0300

    added template for payments-mobile, add show for payments-mobile in profile-mobiile

--- luckydiamond/src/components/adaptive-components/PaymentsMobile.vue
+++ luckydiamond/src/components/adaptive-components/PaymentsMobile.vue
@@ -1,7 +1,64 @@
 <template>
-
+  <div class="root-mobile">
+    <div class="payments-modal-mobile text-style-mobile padding-elements__mobile">
+      <h1>{{ payments.paymentsView ? 'Пополнить' : 'Вывод' }}</h1>
+      <img
+          src="@/assets/icons-other/icon-payments-modal-close.svg"
+          class="icon-close__mobile"
+          @click="closeModal()"
+      >
+    </div>
+    <div class="payments-modal-mobile__content">
+      <h2>Сумма {{ payments.paymentsView ? 'пополнения' : 'вывода' }}</h2>
+      <div class="payments-modal__input">
+        <input type="text">
+        <div class="payments-modal__btns-mobile">
+          <ul>
+            <li v-for="number in PaymentsModalNumbers" :key="number">
+              <button>{{ number.diamonds }}</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="payments-modal__promocode" v-if="payments.paymentsView === true">
+        <input type="text">
+      </div>
+      <div class="payments-modal__agree checkbox-styles">
+        <input type="checkbox" v-model="agreeUser">
+        <h3>Я согласен с пользовательским соглашением.</h3>
+      </div>
+      <captcha-component :showcaptcha="agreeUser"></captcha-component>
+      <div class="payments-modal__submit">
+        <p>Вы подтверждаете правильность введенных данных при создании вывода.</p>
+        <button @click="closeModal()">{{ payments.paymentsView ? 'Пополнить' : 'Вывод' }}</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import PaymentsModalNumbers from "@/mocks/PaymentsModalNumbers";
+import CaptchaComponent from "@/components/CaptchaComponent.vue";
 
+import '@/assets/css/ComponentsStyles/AdaptiveStyles/payments-modalmobile.css'
+
+export default {
+  components: { CaptchaComponent },
+  props: [ 'payments' ],
+  data() {
+    return {
+      PaymentsModalNumbers,
+      agreeUser: false
+    }
+  },
+  methods: {
+    closeModal() {
+      return this.$emit('closemodal')
+    }
+  }
+}
 </script>
+
+<style>
+
+</style>
--- luckydiamond/src/pages/adaptive-pages/ProfilemobilePage.vue
+++ luckydiamond/src/pages/adaptive-pages/ProfilemobilePage.vue
@@ -12,25 +12,43 @@
           <h3 class="text-nickname-user">Artmeka</h3>
           <h3 class="balance-border balance-display balance-text"><img src="@/assets/icons-profile/icon-diamond-ore.png">999</h3>
           <div class="profile-mobile__btns-payments">
-            <a href="#" class="deposit-button"><img class="icon-margin-deposit-withdraw" src="@/assets/icons-profile/icon-deposit.svg"> Пополнить</a>
-            <a href="#" class="withdraw-button"><img class="icon-margin-deposit-withdraw" src="@/assets/icons-profile/icon-withdraw.png"> Вывести</a>
+            <a href="#" @click="paymetsCall('dep')" class="deposit-button"><img class="icon-margin-deposit-withdraw" src="@/assets/icons-profile/icon-deposit.svg"> Пополнить</a>
+            <a href="#" @click="paymetsCall('with')" class="withdraw-button"><img class="icon-margin-deposit-withdraw" src="@/assets/icons-profile/icon-withdraw.png"> Вывести</a>
           </div>
         </div>
       </div>
     </main>
     <menu-mobile-component></menu-mobile-component>
-
+    <payments-mobile @closemodal="paymentsClose" :payments="payments" v-if="payments.paymentsWindow"></payments-mobile>
   </div>
 </template>
 
 <script>
 import HeaderMobileComponent from "@/components/adaptive-components/HeaderMobileComponent.vue";
 import MenuMobileComponent from "@/components/adaptive-components/MenuMobileComponent.vue";
+import PaymentsMobile from "@/components/adaptive-components/PaymentsMobile.vue";
 
 import '@/assets/css/PagesStyles/adaptive-pages/profilemobile.css'
 
 export default {
-  components: { HeaderMobileComponent, MenuMobileComponent }
+  components: { HeaderMobileComponent, MenuMobileComponent, PaymentsMobile },
+  data () {
+    return {
+      payments: {
+        paymentsWindow: false,
+        paymentsView: false
+      },
+    }
+  },
+  methods: {
+    paymetsCall(view) {
+      this.payments.paymentsWindow = true
+      this.payments.paymentsView = view === 'dep'
+    },
+    paymentsClose() {
+      this.payments.paymentsWindow = false
+    }
+  }
 }
 </script>
 
