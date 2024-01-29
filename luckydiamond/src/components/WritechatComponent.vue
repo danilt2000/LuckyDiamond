dead4e8c2ae9ba785b6a80e23061be47dc215502
Hepatica <danil.tkachenko@getmore.cz>
2024-01-29 21:51:56 +0100

    fix InsertSpaceEvery24Chars

--- luckydiamond/src/components/WritechatComponent.vue
+++ luckydiamond/src/components/WritechatComponent.vue
@@ -46,13 +46,16 @@ export default {
       this.$emit("send", [formattedWord, this.username, this.icon]);
       this.msg = "";
     },
-    
-    InsertSpaceEvery24Chars(word) {
+
+    InsertSpaceEvery24Chars(text) {
       let result = "";
-      for (let i = 0; i < word.length; i += 22) {
-        result += word.substring(i, i + 22) + " ";
+      for (let i = 0; i < text.length; i++) {
+        if (i > 0 && i % 24 === 0) {
+          result += " ";
+        }
+        result += text[i];
       }
-      return result.trim();
+      return result;
     },
   },
 };
