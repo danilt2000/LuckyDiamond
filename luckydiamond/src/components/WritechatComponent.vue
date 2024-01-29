ff80ad7185ae5fc2b81e30b57962dd60d438e75a
Hepatica <danil.tkachenko@getmore.cz>
2024-01-29 22:05:15 +0100

    Add word break

--- luckydiamond/src/components/WritechatComponent.vue
+++ luckydiamond/src/components/WritechatComponent.vue
@@ -42,21 +42,21 @@ export default {
       }
     },
     SendmsgData() {
-      const formattedWord = this.InsertSpaceEvery24Chars(this.msg);
-      this.$emit("send", [formattedWord, this.username, this.icon]);
+      // const formattedWord = this.InsertSpaceEvery24Chars(this.msg);
+      this.$emit("send", [this.msg, this.username, this.icon]);
       this.msg = "";
     },
 
-    InsertSpaceEvery24Chars(text) {
-      let result = "";
-      for (let i = 0; i < text.length; i++) {
-        if (i > 0 && i % 24 === 0) {
-          result += " ";
-        }
-        result += text[i];
-      }
-      return result;
-    },
+    // InsertSpaceEvery24Chars(text) {
+    //   let result = "";
+    //   for (let i = 0; i < text.length; i++) {
+    //     if (i > 0 && i % 24 === 0) {
+    //       result += " ";
+    //     }
+    //     result += text[i];
+    //   }
+    //   return result;
+    // },
   },
 };
 </script>
