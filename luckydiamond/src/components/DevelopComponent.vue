19b3d87b5fb1963cb0002f9c8a9a64163dcd42e6
Kostya <ckutls228@gmail.com>
2024-02-01 20:39:55 +0300

    fixed img-style, fixed margin in template

--- luckydiamond/src/components/DevelopComponent.vue
+++ luckydiamond/src/components/DevelopComponent.vue
@@ -1,27 +1,28 @@
 <template>
-    <div class="info">
-        <div class="card">
-<div class="text">
-<div class="aboutinfo">
-<p>фронтэндер</p>
-<h1><b>busha888</b></h1>
-</div>
-<div class="whatjob">
-<img src="@/assets/icons-developcard/html.png" alt="html">
-<p>HTML 5,CSS</p>
-</div>
-<div class="icons">
-<a href="https://tpverstak.ru/flex-cheatsheet/"><img src="@/assets/icons-developcard/odniclasniki.png" alt=""></a>
-<a href=""><img src="" alt=""></a>
-<a href=""><img src="" alt=""></a>
-<a href=""><img src="" alt=""></a>
-</div>
-</div>
-<div class="skin">
-    
-</div>
+  <div class="info">
+    <div class="card">
+      <div class="text">
+        <div class="aboutinfo">
+          <p>фронтэндер</p>
+          <h1><b>busha888</b></h1>
         </div>
+        <div class="whatjob">
+          <img src="@/assets/icons-developcard/html.png" alt="html">
+          <p>HTML 5,CSS</p>
+        </div>
+        <div class="icons">
+          <a href="https://tpverstak.ru/flex-cheatsheet/"><img src="@/assets/icons-developcard/odniclasniki.png" alt=""></a>
+          <a href=""><img src="" alt=""></a>
+          <a href=""><img src="" alt=""></a>
+          <a href=""><img src="" alt=""></a>
+        </div>
+      </div>
     </div>
+  </div>
+<div class="skin">
+
+</div>
+
 </template>
 
 <script>
