e0decfe14aaf3d549c2af52d9c099df3fccf26c2
Madara0330E <summoners2011sss@gmail.com>
2024-02-01 20:25:19 +0300

    cardinfo

--- luckydiamond/src/components/DevelopComponent.vue
+++ luckydiamond/src/components/DevelopComponent.vue
@@ -1,7 +1,25 @@
 <template>
     <div class="info">
         <div class="card">
-
+<div class="text">
+<div class="aboutinfo">
+<p>фронтэндер</p>
+<h1><b>busha888</b></h1>
+</div>
+<div class="whatjob">
+<img src="@/assets/icons-developcard/html.png" alt="html">
+<p>HTML 5,CSS</p>
+</div>
+<div class="icons">
+<a href="https://tpverstak.ru/flex-cheatsheet/"><img src="@/assets/icons-developcard/odniclasniki.png" alt=""></a>
+<a href=""><img src="" alt=""></a>
+<a href=""><img src="" alt=""></a>
+<a href=""><img src="" alt=""></a>
+</div>
+</div>
+<div class="skin">
+    
+</div>
         </div>
     </div>
 </template>
