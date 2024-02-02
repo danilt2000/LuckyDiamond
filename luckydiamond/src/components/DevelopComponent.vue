fa730b1b0b2f9d2a6d38abef3d66acca8faacfdd
Madara0330E <summoners2011sss@gmail.com>
2024-02-02 17:37:10 +0300

    card

--- luckydiamond/src/components/DevelopComponent.vue
+++ luckydiamond/src/components/DevelopComponent.vue
@@ -4,7 +4,7 @@
       <div class="text">
         <div class="aboutinfo">
           <p>фронтэндер</p>
-          <h1><b>busha888</b></h1>
+          <h1>busha888</h1>
         </div>
         <div class="whatjob">
           <img src="@/assets/icons-developcard/html.png" alt="html">
@@ -12,16 +12,29 @@
         </div>
         <div class="icons">
           <a href="https://tpverstak.ru/flex-cheatsheet/"><img src="@/assets/icons-developcard/odniclasniki.png" alt=""></a>
-          <a href=""><img src="" alt=""></a>
-          <a href=""><img src="" alt=""></a>
-          <a href=""><img src="" alt=""></a>
+          <a href=""><img src="@/assets/icons-developcard/telegram.png" alt=""></a>
+          <a href=""><img src="@/assets/icons-developcard/like.png" alt=""></a>
+          <a href=""><img src="@/assets/icons-developcard/github.png" alt=""></a>
+        </div>
+      </div>
+      <div class="skin">
+        <img src="@/assets/icons-developcard/busha888.png" alt="">
+        </div>
+    </div>
+    <div class="card2">
+      <div class="text_2">
+        <div class="aboutinfo_2">
+
+        </div>
+        <div class="whatjob_2">
+
+        </div>
+        <div class="skin_2">
+          
         </div>
       </div>
     </div>
   </div>
-<div class="skin">
-
-</div>
 
 </template>
 
