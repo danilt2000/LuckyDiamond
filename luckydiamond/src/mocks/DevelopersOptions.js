e865f09c0d9b41e3d508e8c1344106632b7f672b
Kostya <ckutls228@gmail.com>
2024-02-03 23:23:26 +0300

    refactor options and styles for developeroptions

--- luckydiamond/src/mocks/DevelopersOptions.js
+++ luckydiamond/src/mocks/DevelopersOptions.js
@@ -1,93 +1,105 @@
 export default [
      {
-        Rafael1209:{
-            username:"Rafael1209",
+        Rafael1209: {
+            username: "Rafael1209",
             userRole: 'Бэкендер',
-            background:"#636363",
-            technologyText:"ASP,.NET,MongoDB,Designer",
-            technologyIcons:{
+            background: "#636363",
+            technologyText: ['ASP,.NET,MongoDB', 'Designer'],
+            technologyIcons: {
                 csharp: "@/assets/icons-developcard/csharp-logo.png",
                 figma: "@/assets/icons-developcard/figma.png"
             },
             socialIcons: {
-                github: "@/assets/icons-developcard/github.png",
+                telegram: "@/assets/icons-developcard/telegram.png",
                 discord: "@/assets/icons-developcard/discord.png",
-                telegram: "@/assets/icons-developcard/telegram.png"
+                github: "@/assets/icons-developcard/github.png"
             }
         },
         Hepatir:{
-            username:"Hepatir",
+            username: "Hepatir",
             userRole: 'Бэкендер',
-            background:"#595959",
-            technologyText:"ASP,.NET,MongoDB,SQL",
-            technologyIcons:{
-                github: "@/assets/icons-developcard/github.png",
-                discord: "@/assets/icons-developcard/discord.png",
+            background: "#413C3B",
+            technologyText: ['ASP,.NET,MongoDB', 'SQL'],
+            technologyIcons: {
                 csharp: "@/assets/icons-developcard/csharp-logo.png",
-                telegram: "@/assets/icons-developcard/telegram.png",
-                pngegg: "@/assets/icons-developcard/sql-logo.png"
+                sql: "@/assets/icons-developcard/sql-logo.png"
             },
-
-        },
-        Ckutls_:{
-            username:"Ckutls_",
-            userRole: 'Фронтэндер',
-            background:"#B88484",
-            technologyText:"HTML 5, CSS,JavaScript,Vue 3",
-            technologyIcons:{
-                github: "@/assets/icons-developcard/github.png",
-                discord: "@/assets/icons-developcard/discord.png",
+            socialIcons: {
                 telegram: "@/assets/icons-developcard/telegram.png",
+                discord: "@/assets/icons-developcard/discord.png",
+                github: "@/assets/icons-developcard/github.png",
+            }
+        },
+        Ckutls_: {
+            username: "Ckutls_",
+            userRole: 'Фронтэндер',
+            background: "#B88484",
+            technologyText: ['html 5, css', 'JavaScript', 'Vue 3'],
+            technologyIcons: {
+                html: '@/assets/icons-developcard/html.png',
                 js: "@/assets/icons-developcard/js.png",
                 vue: "@/assets/icons-developcard/vue.png"
-            }
-        },
-        FUpir:{
-            username:"FUpir",
-            userRole: 'Дизайнер',
-            background:"#C58E74",
-            technologyText:"Designer",
-            technologyIcons:{
-                discord: "@/assets/icons-developcard/discord.png",
+            },
+            socialIcons: {
                 telegram: "@/assets/icons-developcard/telegram.png",
-                figma: "@/assets/icons-developcard/figma.png"
-            }
-        },
-        megatntmega:{
-            username:"megatntmega",
-            userRole: 'Дизайнер',
-            background:"#393A3A",
-            technologyText:"Designer",
-            technologyIcons:{
                 discord: "@/assets/icons-developcard/discord.png",
-                telegram: "@/assets/icons-developcard/telegram.png",
-                Figma: "@/assets/icons-developcard/figma.png"
-            }
-        },
-        busha888:{
-            username:"busha888",
-            userRole: 'Фронтэндер',
-            background:"#742271",
-            technologyText:"HTML 5, CSS",
-            technologyIcons:{
                 github: "@/assets/icons-developcard/github.png",
-                telegram: "@/assets/icons-developcard/telegram.png",
-                odniclasniki: "@/assets/icons-developcard/odniclasniki.png",
-                like: "@/assets/icons-developcard/like.png",
-                html: "@/assets/icons-developcard/html.png"
             }
         },
-        CLARFFY:{
-            username:"CLARFFY",
+        FUpir: {
+            username: "FUpir",
             userRole: 'Дизайнер',
-            background:"#742271",
-            technologyText:"Designer",
-            technologyIcons:{
-                discord: "@/assets/icons-developcard/discord.png",
+            background: "#C58E74",
+            technologyText: ['Designer'],
+            technologyIcons: {
+                figma: "@/assets/icons-developcard/figma.png"
+            },
+            socialIcons: {
                 telegram: "@/assets/icons-developcard/telegram.png",
-                Figma: "@/assets/icons-developcard/figma.png"
+                discord: "@/assets/icons-developcard/discord.png"
             }
         },
+        megatntmega: {
+            username: "megatntmega",
+            userRole: 'Дизайнер',
+            background: "#393A3A",
+            technologyText: ['Designer'],
+            technologyIcons: {
+                figma: "@/assets/icons-developcard/figma.png"
+            },
+            socialIcons: {
+                telegram: "@/assets/icons-developcard/telegram.png",
+                discord: "@/assets/icons-developcard/discord.png"
+            }
+        },
+        busha888: {
+            username: "busha888",
+            userRole: 'Фронтэндер',
+            background: "#742271",
+            technologyText: ['html 5, css'],
+            technologyIcons: {
+                html: "@/assets/icons-developcard/html.png"
+            },
+            socialIcons: {
+                odniclasniki: "@/assets/icons-developcard/odniclasniki.png",
+                telegram: "@/assets/icons-developcard/telegram.png",
+                like: "@/assets/icons-developcard/like.png",
+                github: "@/assets/icons-developcard/github.png",
+            }
+        },
+        CLARFFY: {
+            username: "CLARFFY",
+            userRole: 'Дизайнер',
+            background: "#E2B374",
+            technologyText: ['Designer'],
+            technologyIcons: {
+                figma: "@/assets/icons-developcard/figma.png"
+            },
+            socialIcons: {
+                telegram: "@/assets/icons-developcard/telegram.png",
+                discord: "@/assets/icons-developcard/discord.png"
+            }
+        }
    }
 ]
     
