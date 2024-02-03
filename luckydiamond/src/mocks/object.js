2f87c5628d4ae6885cfa96afedcb12fb8d942b59
Madara0330E <summoners2011sss@gmail.com>
2024-02-03 21:16:53 +0300

    addtab

--- luckydiamond/src/mocks/object.js
+++ luckydiamond/src/mocks/object.js
@@ -1,10 +1,10 @@
 export default [
-   {
-    Rafael1209:{
-        username:"Rafael1209",
-        background:"#636363",
-        tehnologi_text:"ASP,.NET,MongoDB,Designer",
-        icons_tehnologi:{
+     {
+        Rafael1209:{
+            username:"Rafael1209",
+            background:"#636363",
+            tehnologi_text:"ASP,.NET,MongoDB,Designer",
+                icons_tehnologi:{
             github: "@/assets/icons-developcard/github.png",
             discord: "@/assets/icons-developcard/discord.png",
             telegram: "@/assets/icons-developcard/telegram.png",
@@ -34,51 +34,51 @@ export default [
         telegram: "@/assets/icons-developcard/telegram.png",
         js: "@/assets/icons-developcard/js.png",
         Vue: "@/assets/icons-developcard/Vue.png"
-    }
-},
-FUpir:{
-    username:"FUpir",
-    background:"#C58E74",
-    tehnologi_text:"Designer",
-    icons_tehnologi:{
-        discord: "@/assets/icons-developcard/discord.png",
-        telegram: "@/assets/icons-developcard/telegram.png",
-        Figma: "@/assets/icons-developcard/Figma.png"
-    }
-},
-megatntmega:{
-    username:"megatntmega",
-    background:"#393A3A",
-    tehnologi_text:"Designer",
-    icons_tehnologi:{
-        discord: "@/assets/icons-developcard/discord.png",
-        telegram: "@/assets/icons-developcard/telegram.png",
-        Figma: "@/assets/icons-developcard/Figma.png"
-    }
-},
-busha888:{
-    username:"busha888",
-    background:"#742271",
-    tehnologi_text:"HTML 5, CSS",
-    icons_tehnologi:{
-        github: "@/assets/icons-developcard/github.png",
-        telegram: "@/assets/icons-developcard/telegram.png",
-        odniclasniki: "@/assets/icons-developcard/odniclasniki.png",
-        like: "@/assets/icons-developcard/like.png",
-        html: "@/assets/icons-developcard/html.png"
-    }
-},
-CLARFFY:{
-    username:"CLARFFY",
-    background:"#742271",
-    tehnologi_text:"Designer",
-    icons_tehnologi:{
-        discord: "@/assets/icons-developcard/discord.png",
-        telegram: "@/assets/icons-developcard/telegram.png",
-        Figma: "@/assets/icons-developcard/Figma.png"
+        }
+    },
+    FUpir:{
+        username:"FUpir",
+        background:"#C58E74",
+        tehnologi_text:"Designer",
+        icons_tehnologi:{
+            discord: "@/assets/icons-developcard/discord.png",
+            telegram: "@/assets/icons-developcard/telegram.png",
+            Figma: "@/assets/icons-developcard/Figma.png"
+        }
+    },
+    megatntmega:{
+        username:"megatntmega",
+        background:"#393A3A",
+        tehnologi_text:"Designer",
+        icons_tehnologi:{
+            discord: "@/assets/icons-developcard/discord.png",
+            telegram: "@/assets/icons-developcard/telegram.png",
+            Figma: "@/assets/icons-developcard/Figma.png"
+        }
+    },
+    busha888:{
+        username:"busha888",
+        background:"#742271",
+        tehnologi_text:"HTML 5, CSS",
+        icons_tehnologi:{
+            github: "@/assets/icons-developcard/github.png",
+            telegram: "@/assets/icons-developcard/telegram.png",
+            odniclasniki: "@/assets/icons-developcard/odniclasniki.png",
+            like: "@/assets/icons-developcard/like.png",
+            html: "@/assets/icons-developcard/html.png"
+        }
+    },
+    CLARFFY:{
+        username:"CLARFFY",
+        background:"#742271",
+        tehnologi_text:"Designer",
+        icons_tehnologi:{
+            discord: "@/assets/icons-developcard/discord.png",
+            telegram: "@/assets/icons-developcard/telegram.png",
+            Figma: "@/assets/icons-developcard/Figma.png"
 
-    }
-},
+        }
+    },
    }
 ]
     
