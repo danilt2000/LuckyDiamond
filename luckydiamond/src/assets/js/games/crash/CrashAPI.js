24a743fefab1049429206e52a941215e3283efd3
Kostya <ckutls228@gmail.com>
2024-02-04 16:15:44 +0300

    added crash-api

---
+++ luckydiamond/src/assets/js/games/crash/CrashAPI.js
@@ -0,0 +1,63 @@
+import { BackendApiUrl } from '@/properties/Сonfig.js';
+
+export async function JoinCrashGame(userData, amount) {
+    const data = {
+        userCredentials: {
+            searchToken: userData.searchToken,
+            authtoken: userData.authtoken
+        },
+        bid: amount
+    }
+
+    try {
+        const response = await fetch(`${BackendApiUrl}/GameCrash/JoinCrashGame`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data),
+            redirect: 'follow'
+        })
+
+        if (!response.ok) {
+            console.log('Fetch error:', response.status)
+        }
+
+        console.log(response)
+        return await response.json()
+    }
+    catch (error) {
+        console.log('Fetch error')
+    }
+}
+
+export async function ExitAndTakeMoneyFromCrashGame(userData) {
+    const data = {
+        exitUserCredentials: {
+            searchToken: userData.searchToken,
+            authtoken: userData.authtoken
+        }
+    }
+
+    try {
+        const response = await fetch(`${BackendApiUrl}/GameCrash/ExitAndTakeMoneyFromCrash`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data),
+            redirect: 'follow'
+        })
+
+        if (!response.ok) {
+            console.log('Fetch error:', response.status)
+        }
+
+        console.log(response)
+        return await response.json()
+    }
+    catch (error) {
+        console.log('Fetch error')
+    }
+
+}
