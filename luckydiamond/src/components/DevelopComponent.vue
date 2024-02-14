9b3262bcb834b0da649cfa47377d56bb6ec50b7d
Madara0330E <summoners2011sss@gmail.com>
2024-02-14 20:14:06 +0300

    God, I think the Developer did it.

--- luckydiamond/src/components/DevelopComponent.vue
+++ luckydiamond/src/components/DevelopComponent.vue
@@ -1,6 +1,133 @@
 <template>
   <div class="info">
-    <div class="card">
+    <div class="top_card">
+    <div class="Rafael1209_card">
+      <div class="Rafael1209_text_2">
+        <div class="Rafael1209_aboutinfo_2">
+          <p>Бэкендер</p>
+          <h1>Rafael1209</h1>
+        </div>
+        <div class="Rafael1209_whatjob_2">
+          <img src="@/assets/icons-developcard/Logo_C_sharp.png" alt="html">
+          <p>ASP, .NET, MongoDB</p>
+        </div>
+        <div class="Rafael1209_whatjob2_2">
+          <img src="@/assets/icons-developcard/Figma.png" alt="html">
+          <p>Designer</p>
+        </div>
+        </div>
+        <div class="Rafael1209_skin_2">
+          <div class="Rafael1209_skin_2_skin">
+            
+            <div class="Rafael1209_skinicons1">
+            <a href=""><img src="@/assets/icons-developcard/telegram.png" alt=""></a>
+            <a href=""><img src="@/assets/icons-developcard/discord.png" alt=""></a>
+            <a href=""><img src="@/assets/icons-developcard/github.png" alt=""></a>
+            </div>
+          </div>
+        </div>
+    </div>
+    <div class="Hepatir_card">
+      <div class="text_2">
+        <div class="aboutinfo_2">
+          <p>Бэкендер</p>
+          <h1>Hepatir</h1>
+        </div>
+        <div class="whatjob_2">
+          <img src="@/assets/icons-developcard/Logo_C_sharp.png" alt="html">
+          <p>ASP, .NET, MongoDB</p>
+        </div>
+        <div class="whatjob2_2">
+          <img src="@/assets/icons-developcard/pngegg.png" alt="html">
+          <p>SQL</p>
+        </div>
+        </div>
+        <div class="skin_2">
+          <div class="skin_2_skin">
+            <div class="skinicons1">
+            <a href=""><img src="@/assets/icons-developcard/telegram.png" alt=""></a>
+            <a href=""><img src="@/assets/icons-developcard/discord.png" alt=""></a>
+            <a href=""><img src="@/assets/icons-developcard/github.png" alt=""></a>
+            </div>
+          </div>
+        </div>
+    </div>
+    <div class="Ckutls__card">
+      <div class="Ckutls__text_2">
+        <div class="Ckutls__aboutinfo_2">
+          <p>Бэкендер</p>
+          <h1>Ckutls_</h1>
+        </div>
+        <div class="Ckutls__whatjob_1">
+          <img src="@/assets/icons-developcard/html.png" alt="html">
+          <p>HTML 5,CSS</p>
+        </div>
+        <div class="Ckutls__whatjob_2">
+          <img src="@/assets/icons-developcard/js.png" alt="html">
+          <p>JavaScript</p>
+        </div>
+        <div class="Ckutls__whatjob_3">
+          <img src="@/assets/icons-developcard/Vue.png" alt="html">
+          <p>Vue 3</p>
+        </div>
+        </div>
+        <div class="Ckutls__skin_2">
+          <div class="Ckutls__skin_2_skin">
+            
+            <div class="Ckutls__skinicons1">
+            <a href=""><img src="@/assets/icons-developcard/telegram.png" alt=""></a>
+            <a href=""><img src="@/assets/icons-developcard/discord.png" alt=""></a>
+            <a href=""><img src="@/assets/icons-developcard/github.png" alt=""></a>
+            </div>
+          </div>
+        </div>
+      
+    </div>
+    <div class="FUpir_card">
+      <div class="FUpir_text_2">
+        <div class="FUpir_aboutinfo_2">
+          <p>Дизайнер</p>
+          <h1>FUpir</h1>
+        </div>
+        <div class="FUpir_whatjob_1">
+          <img src="@/assets/icons-developcard/Figma.png" alt="html">
+          <p>Designer</p>
+        </div>
+        </div>
+        <div class="FUpir_skin_2">
+          <div class="FUpir_skin_2_skin">
+            
+            <div class="FUpir_skinicons1">
+            <a href=""><img src="@/assets/icons-developcard/telegram.png" alt=""></a>
+            <a href=""><img src="@/assets/icons-developcard/discord.png" alt=""></a>
+           
+            </div>
+          </div>
+        </div>
+      
+    </div>
+  </div>
+  <div class="bottom_card">
+    <div class="megatntmega_card">
+      <div class="megatntmega_text">
+        <div class="megatntmega_aboutinfo">
+          <p>Дизайнер</p>
+          <h1>megatntmega</h1>
+        </div>
+        <div class="megatntmega_whatjob">
+          <img src="@/assets/icons-developcard/Figma.png" alt="html">
+          <p>Designer</p>
+        </div>
+        <div class="megatntmega_icons">
+          <a href=""><img src="@/assets/icons-developcard/telegram.png" alt=""></a>
+          <a href=""><img src="@/assets/icons-developcard/discord.png" alt=""></a>
+        </div>
+      </div>
+      <div class="megatntmega_skin">
+        <img src="@/assets/icons-developcard/megatntmega.png" alt="">
+        </div>
+    </div>
+    <div class="busha888_card">
       <div class="text">
         <div class="aboutinfo">
           <p>фронтэндер</p>
@@ -21,33 +148,26 @@
         <img src="@/assets/icons-developcard/busha888.png" alt="">
         </div>
     </div>
-    <div class="card-2">
-      <div class="text_2">
-        <div class="aboutinfo_2">
-          <p>Бэкендер</p>
-          <h1>Hepatir</h1>
+    <div class="CLARFFY_card">
+      <div class="CLARFFY_text">
+        <div class="CLARFFY_aboutinfo">
+          <p>Дизайнер</p>
+          <h1>CLARFFY</h1>
         </div>
-        <div class="whatjob_2">
-          <img src="@/assets/icons-developcard/Logo_C_sharp.png" alt="html">
-          <p>ASP, .NET, MongoDB</p>
+        <div class="CLARFFY_whatjob">
+          <img src="@/assets/icons-developcard/Figma.png" alt="html">
+          <p>Designer</p>
         </div>
-        <div class="whatjob2_2">
-          <img src="@/assets/icons-developcard/pngegg.png" alt="html">
-          <p>SQL</p>
+        <div class="CLARFFY_icons">
+          <a href=""><img src="@/assets/icons-developcard/telegram.png" alt=""></a>
+          <a href=""><img src="@/assets/icons-developcard/discord.png" alt=""></a>
         </div>
+      </div>
+      <div class="CLARFFY_skin">
+        <img src="@/assets/icons-developcard/CLARFFY.png" alt="">
         </div>
-        <div class="skin_2">
-          <div class="skin_2_skin">
-            
-            <div class="skinicons1">
-            <a href=""><img src="@/assets/icons-developcard/telegram.png" alt=""></a>
-            <a href=""><img src="@/assets/icons-developcard/discord.png" alt=""></a>
-            <a href=""><img src="@/assets/icons-developcard/github.png" alt=""></a>
-            </div>
-          </div>
-        </div>
-      
     </div>
+  </div>
   </div>
 
 </template>
