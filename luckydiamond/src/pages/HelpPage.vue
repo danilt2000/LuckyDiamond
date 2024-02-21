8d2202c39336fd0aca1da65dbe40779abe0193bc
Hepatica <danil.tkachenko@getmore.cz>
2024-02-21 19:27:31 +0100

    fix help page for madara

--- luckydiamond/src/pages/HelpPage.vue
+++ luckydiamond/src/pages/HelpPage.vue
@@ -1,36 +1,35 @@
 <template>
-    <div class="content-grid--help">
-      <aside-bar-component></aside-bar-component>
-  
-      <section class="help">
-  
-      <develop-component></develop-component>
-  
-      </section>
-      <header-component></header-component>
-  
-    </div>
-  </template>
+  <div class="content-grid--help">
+    <aside-bar-component></aside-bar-component>
+
+    <develop-component></develop-component>
+    <header-component></header-component>
+
+    <section class="help">
+      <h1>gfsdlkgdfk</h1>
+    </section>
+  </div>
+</template>
   <script>
-  import HelpComponent from "@/components/HelpComponent.vue";
-  import AsideBarComponent from "@/components/AsidebarComponent.vue";
-  import HeaderComponent from "@/components/HeaderComponent.vue";
-  
-  export default {
-    data() {
-      return {
-        Object: {
-          bg: "value",
-          nickname: "value",
-          icons: {},
-          textTacholgy: { text1: "text2" },
-        },
-      };
-    },
-    components: {
-      HeaderComponent,
-      AsideBarComponent,
-      HelpComponent,
-    },
-  };
-  </script>
+// import HelpComponent from "@/components/HelpComponent.vue";
+import AsideBarComponent from "@/components/AsidebarComponent.vue";
+import HeaderComponent from "@/components/HeaderComponent.vue";
+
+export default {
+  data() {
+    return {
+      Object: {
+        bg: "value",
+        nickname: "value",
+        icons: {},
+        textTacholgy: { text1: "text2" },
+      },
+    };
+  },
+  components: {
+    HeaderComponent,
+    AsideBarComponent,
+    // HelpComponent,
+  },
+};
+</script>
