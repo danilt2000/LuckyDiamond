55f9e9c2b9fadf6385fc70d3c262240f290a591d
Hepatica <danil.tkachenko@getmore.cz>
2024-03-02 21:21:28 +0100

    change icons

--- luckydiamond/src/components/AsidebarComponent.vue
+++ luckydiamond/src/components/AsidebarComponent.vue
@@ -6,7 +6,8 @@
       </div>
       <div class="menu__btns--gamemodes">
         <div class="menu__content--gamemodes">
-          <div class="fotosloti"><a href="#"><img  src="../assets/icons-menu/slots-icon.svg" ></a></div>
+          <!-- <div class="fotosloti"><a href="#"><img  src="../assets/icons-menu/slots-icon.svg" ></a></div> -->
+          <a href="#" @click="$router.push({ name: 'jackpot' })"><img  class="menu__content--jackpot-icon" src="../assets/icons-menu/jackpot-icon.svg"></a>
           <a href="#" @click="$router.push({ name: 'crash' })"><img  class="menu__content--cruch-icon" src="../assets/icons-menu/crush-icon.svg"></a>
           <a href="#"><img  src="../assets/icons-menu/case-icon.png"></a>
            <!-- <a href="#" @click="$router.push({ name: 'saper' })"><img  class="menu__content--bomb-icon" src="../assets/icons-menu/bomb-icon.svg"></a> -->
