f9abb597beccbb6f12283a39625f5b15a7759331
Kostya <ckutls228@gmail.com>
2024-03-05 19:51:48 +0300

    fixed margin in menu-mobile, fixed home-text

--- luckydiamond/src/components/adaptive-components/MenuMobileComponent.vue
+++ luckydiamond/src/components/adaptive-components/MenuMobileComponent.vue
@@ -2,11 +2,11 @@
   <footer class="footer-mobile">
     <div class="footer-mobile__content">
       <div class="footer-mobile__icons icons-size">
-        <a href="#"><img class="icon-start" src="@/assets/icons-adaptive/home-adaptive/icon-home-menu.png"></a>
-        <a href="#" @click="$router.push({ name: 'profile' })"><img class="icon-center" src="@/assets/icons-adaptive/home-adaptive/icon-wallet-menu.svg"></a>
-        <a href="#"><img class="icon-center" src="@/assets/icons-adaptive/home-adaptive/icon-chat-menu.png"></a>
-        <a href="#"><img class="icon-center" src="@/assets/icons-adaptive/home-adaptive/icon-info-menu.png"></a>
-        <a href="#"><img class="icon-end" src="@/assets/icons-adaptive/home-adaptive/icon-menu.png"></a>
+        <a class="icon-center" href="#"><img @click="$router.push({ name: 'home' })" src="@/assets/icons-adaptive/home-adaptive/icon-home-menu.png"></a>
+        <a class="icon-center" href="#" @click="$router.push({ name: 'profile' })"><img src="@/assets/icons-adaptive/home-adaptive/icon-wallet-menu.svg"></a>
+        <a class="icon-center" href="#"><img src="@/assets/icons-adaptive/home-adaptive/icon-chat-menu.png"></a>
+        <a class="icon-center" href="#"><img src="@/assets/icons-adaptive/home-adaptive/icon-info-menu.png"></a>
+        <a class="icon-center" href="#"><img src="@/assets/icons-adaptive/home-adaptive/icon-menu.png"></a>
       </div>
     </div>
   </footer>
