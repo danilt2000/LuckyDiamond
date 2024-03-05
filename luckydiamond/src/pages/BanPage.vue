674e1635d2b5cb6cefaa1a0d6fb3749664966227
Swino4ka <sasakvartuk@gmail.com>
2024-03-05 21:23:57 +0100

    Added ban page, fixed animation
    Ну крч накидал страницу бана, цвета, текст взяты с лаки даймонда, кнопку откорректировал, а то мне не нравилось, как она себя ведёт

---
+++ luckydiamond/src/pages/BanPage.vue
@@ -0,0 +1,20 @@
+<template>
+
+<body>
+    <div class="first">
+    <center><h1>Вы были заблокированны на проекте LuckyDiamond</h1></center>
+    <center><iframe class="ggg" width="560" height="315" src="https://www.youtube.com/embed/XeoS-zsGVCs?si=zLlKqDQIzvqOzFjW" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></center>
+</div>
+</body>
+
+</template>
+  
+  
+  <script>
+  import '@/assets/css/PagesStyles/BanPage.css'
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  
