aeb54e2b31ce98e5724c9fba51e9c69a82c7fd4e
Swino4ka <sasakvartuk@gmail.com>
2024-03-09 20:43:04 +0100

    Anymore more

--- luckydiamond/src/pages/BanPage.vue
+++ luckydiamond/src/pages/BanPage.vue
@@ -1,9 +1,9 @@
 <template>
 
-<body class="banbanbimbim">
-    <div class="first">
+<body class="banpage--body">
+    <div class="banpage--block">
     <center><h1>Вы были заблокированны на проекте LuckyDiamond</h1></center>
-    <center><iframe class="ggg" width="560" height="315" src="https://www.youtube.com/embed/XeoS-zsGVCs?si=zLlKqDQIzvqOzFjW" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></center>
+    <center><iframe class="banpage--video" width="560" height="315" src="https://www.youtube.com/embed/XeoS-zsGVCs?si=zLlKqDQIzvqOzFjW" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></center>
 </div>
 </body>
 
