c0017f0b81843003abd5cf2929777b25cdef623e
Swino4ka <sasakvartuk@gmail.com>
2024-03-11 21:25:08 +0100

    Modal for refferals
    Модал для реффералов бахнул, спать надо, поэтому не сделал вёрстку сорян

---
+++ luckydiamond/src/components/ReferalModal.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="payment-modal withdraw-modal">
+      <div class="payments-modal__content close-modal__icon text-styles__margin">
+        <h1>Рефералы</h1>
+        <img @click="closeModal" src="@/assets/icons-other/icon-payments-modal-close.svg" />
+      </div>
+      <div v-if="payments" class="payments-modal__deposit">
+      </div>
+      <div v-else class="payments-modal__withdraw deposit-text withdraw-input">
+        <div class="btn-withdraw btn-text-style btn-display-deposit btn-style-payments">
+          <h1>В РАЗРАБОТКЕ</h1>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import "@/assets/css/ComponentsStyles/payments-modal.css";
+  export default {
+    props: ["payments"],
+    methods: {
+      closeModal() {
+        return this.$emit("closemodal");
+      },
+    },
+  };
+  </script>
+  
