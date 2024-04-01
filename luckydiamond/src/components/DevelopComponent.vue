f3cad0278c84d58f0e27ea67d34d8aefc4ae3ad1
rafael1209 <mr.chasmanrafael@gmail.com>
2024-04-02 00:11:03 +0300

    Revert "Update DevelopComponent.vue"
    This reverts commit acf06941fa8a6e982d84b8c477f1e58b59b3415b.

--- luckydiamond/src/components/DevelopComponent.vue
+++ luckydiamond/src/components/DevelopComponent.vue
@@ -109,12 +109,12 @@
     <div class="Hepatir_card">
       <div class="text_2">
         <div class="aboutinfo_2">
-          <p>NOTWORKER</p>
+          <p>ДИРЕКТОР</p>
           <h1>Swino4ka</h1>
         </div>
         <div class="whatjob_2">
           <img src="@/assets/icons-developcard/Logo_C_sharp.png" alt="html">
-          <p>MAIN KAKAHA</p>
+          <p>ВСЕ ЯЗЫКИ</p>
         </div>
         <div class="whatjob2_2">
           <img src="@/assets/icons-developcard/sql-logo.png" alt="html">
