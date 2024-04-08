2d65e4cd74d6179c4e6758bd84a021842b67141c
rafael1209 <mr.chasmanrafael@gmail.com>
2024-04-08 21:05:36 +0300

    Revert "Merge pull request #21 from danilt2000/DevelopCkutls"
    This reverts commit 25c8bab94f89ecd2fa4a37c4cd5ca1971839d79a, reversing
    changes made to 3b06b7ef2dba5f2bfa5b09b1f8ad38dd10429464.

--- luckydiamond/src/components/adaptive-components/MenuMobileComponent.vue
+++ luckydiamond/src/components/adaptive-components/MenuMobileComponent.vue
@@ -2,11 +2,11 @@
   <footer class="footer-mobile">
     <div class="footer-mobile__content">
       <div class="footer-mobile__icons icons-size">
-        <a class="icon-center" @click="$router.push({ name: 'home' })" href="#"><img src="@/assets/icons-adaptive/home-adaptive/icon-home-menu.png"></a>
-        <a :class="{ 'disabled': cokiehave }" @click="$router.push({ name: 'profile' })" class="icon-center" href="#"><img src="@/assets/icons-adaptive/home-adaptive/icon-wallet-menu.svg"></a>
-        <a class="icon-center" href="#"><img src="@/assets/icons-adaptive/home-adaptive/icon-chat-menu.png"></a>
-        <a class="icon-center" href="#"><img src="@/assets/icons-adaptive/home-adaptive/icon-info-menu.png"></a>
-        <a class="icon-center" href="#"><img src="@/assets/icons-adaptive/home-adaptive/icon-menu.png"></a>
+        <a href="#"><img class="icon-start" src="@/assets/icons-adaptive/home-adaptive/icon-home-menu.png"></a>
+        <a href="#" @click="$router.push({ name: 'profile' })"><img class="icon-center" src="@/assets/icons-adaptive/home-adaptive/icon-wallet-menu.svg"></a>
+        <a href="#"><img class="icon-center" src="@/assets/icons-adaptive/home-adaptive/icon-chat-menu.png"></a>
+        <a href="#"><img class="icon-center" src="@/assets/icons-adaptive/home-adaptive/icon-info-menu.png"></a>
+        <a href="#"><img class="icon-end" src="@/assets/icons-adaptive/home-adaptive/icon-menu.png"></a>
       </div>
     </div>
   </footer>
@@ -14,24 +14,11 @@
 
 <script>
 import '@/assets/css/ComponentsStyles/AdaptiveStyles/menumobile.css'
-import { GetCookie } from "@/assets/js/storage/CookieStorage";
-
 export default {
-  data() {
-    return {
-      cokiehave: true
-    }
-  },
-  created() {
-    const auth = GetCookie('AUTHTOKEN')
-    const search = GetCookie('SearchToken')
 
-    if (auth && search) {
-      this.cokiehave = false
-    }
-  }
 }
 </script>
 
 <style scoped>
+
 </style>
