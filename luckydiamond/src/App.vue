c90f249eb69cbc217140b624715dae10a7ea43f8
rafael1209 <mr.chasmanrafael@gmail.com>
2024-04-17 17:39:02 +0300

    fix

--- luckydiamond/src/App.vue
+++ luckydiamond/src/App.vue
@@ -5,23 +5,12 @@
 </template>
 
 <script>
+
 export default {
   name: 'App',
-  components: {},
-  metaInfo: {
-    title: 'LUCKY DIAMOND',
-    meta: [
-      { property: 'og:title', content: 'LUCKY DIAMOND' },
-      { property: 'og:type', content: 'website' },
-      { property: 'og:url', content: 'https://yourwebsite.com' }, // Замените на ваш URL
-      { property: 'og:image', content: 'https://link.to.your/image.jpg' }, // Замените на URL вашего изображения
-      { property: 'og:description', content: 'MINECRAFT CASINO - SPM' }
-      // Добавьте другие мета-теги Open Graph по вашему выбору
-    ]
-  }
+  components: {}
 }
 </script>
 
 <style>
-/* Ваши стили здесь */
 </style>
