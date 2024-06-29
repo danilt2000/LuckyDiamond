780e761e82728b62664ee3a787060e10e0ed188b
rafael1209 <mr.chasmanrafael@gmail.com>
2024-06-30 00:13:55 +0300

    changed help page
    added a discord widget to help page

--- luckydiamond/src/components/HelpComponent.vue
+++ luckydiamond/src/components/HelpComponent.vue
@@ -1,56 +1,39 @@
-
 <template>
-    <div class="box_help">
-        <div class="Heading_p1">
-        <div class="heding2"><h3>Добро пожаловать на Lucky Diamond Casino</h3></div>
-        <div class="heding2_text">
-        <p><b>Проект "Lucky Diamond Casino" - Представляет собой сайт онлайн-казино на игровую валюту Майнкрафт сервера СПМ.
-          В дальнейшем времени возможно будет и на сервере СП.</b></p>
-          </div>
-        </div>
-          <div class="Heading_p1">
-            <div class="heding2"><h3>Поддержка</h3></div>
-            <div class="heding2_text">
-            <p><b>При вопросах или проблемах на сайте обращайтесь в поддержку которая находиться в <a href="https://discord.gg/BwKrmxQbp9">Discord</a></b></p>
-                </div>
-                <div class="heding2"><h3>Спасибо за помощь</h3></div>
-            <div class="heding2_text">
-            <p><b>Отдельно хочется выразить благодарность Tsybry и всем кто помогал нам в этом не легком пути</b></p>
-            <p><b></b></p>
-                </div>
-            </div>
-              <!-- <div class="questions">
-                <div class="questions_box">
-                <h3>Спасибо за помощь</h3>
-              </div> -->
-                <!-- <div class="questions1">
-                  <b>Вилкой в глаз, или маму твою ебал?</b>
-                  <img src="@\assets\icons-test\lest.png" alt="">
-                </div> -->
-                <!-- <div class="text_questions">
-                 <p><b>Отдельно хочется выразить благодарность всем кто помогал нам в этом не легком пути</b></p> 
-                </div> -->
-                <!-- <div class="questions2">
-                  <b>Вилкой в глаз, или маму твою ебал?</b>
-                  <img src="@\assets\icons-test\lest.png" alt="">
-                </div> -->
-                <div class="Social1where">
-                  <div class="Social1text">
-                    <b>Наши соц-сети:</b>
-                  </div>
-                  <div class="Social1icons">
-                    <a href="https://t.me/lucky_diamond_spm" target="_blank" ><img src="@/assets/icons-developcard/telegram.png" alt=""></a>
-                    <a href="https://discord.gg/BwKrmxQbp9" target="_blank" ><img src="@/assets/icons-developcard/discord.png" alt=""></a>
-                  </div>
-                </div>
-
-              </div>
+  <div class="box_help">
+    <div class="Heading_p1">
+      <div class="heding2"><h3>Lucky Diamond Casino</h3></div>
+      <div class="heding2_text">
+        <p><b>Проект "Lucky Diamond Casino" - Представляет собой сайт онлайн-казино на игровую валюту Майнкрафт сервера СПМ.</b></p>
+      </div>
+      <div class="heding5_text">
+        <p><b>При вопросах или проблемах на сайте обращайтесь в поддержку в Discord</b></p>
+      </div>
+    </div>
+    <div class="Heading_p1">
+      <div class="discord-widget-container">
+        <iframe src="https://discord.com/widget?id=1175432324603719774&theme=dark" 
+                width="350" height="500" allowtransparency="true" frameborder="0" 
+                sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts">
+        </iframe>
+      </div>
+    </div>
+    <div class="Social1where">
+      <div class="Social1icons">
+        <a href="https://t.me/lucky_diamond_spm" target="_blank">
+          <img src="@/assets/icons-developcard/telegram.png" alt="Telegram">
+        </a>
+        <a href="https://discord.gg/BwKrmxQbp9" target="_blank">
+          <img src="@/assets/icons-developcard/discord.png" alt="Discord">
+        </a>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import '@/assets/css/ComponentsStyles/Help.css'
 
 export default {
-
+  // ... (your component logic, if any)
 }
 </script>
